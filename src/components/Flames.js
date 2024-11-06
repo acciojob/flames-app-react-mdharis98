6b1bad6ef9472e5667ff945e816ebde4c7e165b8
Haris <mohammadharis7871@gmail.com>
2024-11-06 16:21:54 +0530

    correct the code

--- src/components/Flames.js
+++ src/components/Flames.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 const Flames = () => {
     const [name1, setName1] = useState("");
     const [name2, setName2] = useState("");
-    const [answere, setAnswere] = useState("");
+    const [answer, setAnswer] = useState("");
     const getRemainingLength = (n1, n2) => {
         let arr1 = n1.split("");
         let arr2 = n2.split("");
@@ -24,25 +24,27 @@ const Flames = () => {
         }
         const flames = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
         const remainingLength = getRemainingLength(name1, name2);
-        const resultIdx = remainingLength % 6;
-        setAnswere(flames[resultIdx]);
+        // const resultIdx = remainingLength % 6;
+        const resultIdx = remainingLength % flames.length;
+
+        setAnswer(flames[resultIdx]);
     }
     const handleClear = (event) => {
         event.preventDefault();
         setName1("");
         setName2("");
-        setAnswere("");
+        setAnswer("");
     }
     return (
         <div>
             <h1>FLAMES GAME</h1>
             <form>
-                <input type="text" name="name1" data-testid="input1" value={name1} onChange={(event) => setName1(event.target.value)}/>
-                <input type="text" name="name2" data-testid="input2" value={name2} onChange={(event) => setName2(event.target.value)}/>
-                <button data-testid="calculate_relationship" onClick={handleCalculateRelation}>Calculate Relationship Future</button>
-                <button data-testid="clear" onClick={handleClear}>Clear</button>
+                <input type="text" name="name1" data-testid="input1" aria-label="name1-input" value={name1} onChange={(event) => setName1(event.target.value)}/>
+                <input type="text" name="name2" data-testid="input2" aria-label="name2-input" value={name2} onChange={(event) => setName2(event.target.value)}/>
+                <button data-testid="calculate_relationship"  aria-label="calculate-relationship" onClick={handleCalculateRelation}>Calculate Relationship Future</button>
+                <button data-testid="clear" aria-label="clear-button" onClick={handleClear}>Clear</button>
             </form>
-            <h3 data-testid="answer">{answere.length > 0 && `${answere}`}</h3>
+            <h3 data-testid="answer"  aria-label="answer-output" >{answer.length > 0 && `${answer}`}</h3>
         </div>
     )
 }
